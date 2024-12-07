68618a0f37bf93d8bf7a5ed0e1f291c54de49825
Eichi2514 <picon1317@gmail.com>
2024-12-07 22:11:04 +0900

    프로그래머스) 이상한 문자 만들기

--- main.cpp
+++ main.cpp
@@ -1,11 +1,32 @@
+#include <string>
+
+using namespace std;
+
+string solution(string s) {
+    int idx = 0;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == ' ') {
+            idx = 0;
+            continue;
+        }
+        if (idx % 2) {
+            s[i] = tolower(s[i]);
+        } else {
+            s[i] = toupper(s[i]);
+        }
+        idx++;
+    }
+    return s;
+}
+
 #include <iostream>
 
 int main() {
+    string test1 = "try hello world";
+    string test2 = "this is a test";
 
-
-    for (int i = 1; i <= 5; i++) {
-        std::cout << "i = " << i << std::endl;
-    }
+    cout << "Test 1 : " << solution(test1) << endl;
+    cout << "Test 2 : " << solution(test2) << endl;
 
     return 0;
 }
