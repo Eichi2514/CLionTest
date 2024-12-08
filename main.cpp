4d8e5872c3482250dfe72a1a723446d82a2fcc3a
Eichi2514 <picon1317@gmail.com>
2024-12-08 16:08:10 +0900

    프로그래머스) 두 개 뽑아서 더하기

--- main.cpp
+++ main.cpp
@@ -1,21 +1,23 @@
-#include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-vector<int> solution(string s) {
-    vector<int> answer = vector<int>(s.length());
-    answer[0] = -1;
-    for (int i = 1; i < s.length(); i++) {
-        for (int j = 0; j < i; j++) {
-            if (s[i] == s[j]) {
-                answer[i] = i-j;
-            }
-        }
-        if (answer[i] == 0) {
-            answer[i] = -1;
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
+
+    for (int i = 0; i < numbers.size(); i++) {
+        for (int j = i + 1; j < numbers.size(); j++) {
+            answer.push_back(numbers[i]+numbers[j]);
         }
     }
+
+    sort(answer.begin(), answer.end());
+
+    auto last = unique(answer.begin(), answer.end());
+
+    answer.erase(last, answer.end());
+
     return answer;
 }
 
@@ -24,14 +26,27 @@ vector<int> solution(string s) {
 int main() {
     // string test1 = "try hello world";
     // string test2 = "this is a test";
-    // vector<int> test1 = {-3, -2, -1, 0, 1, 2, 3};
-    // vector<int> test2 = {2, 2, 3, 3};
+    vector<int> test1 = {2,1,3,4,1};
+    vector<int> test2 = {5,0,2,7};
     // vector<vector<int> > test1 = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
     // vector<vector<int> > test2 = {{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}};
 
 
-    cout << "Test 1 : " << solution("banana") << endl;
-    cout << "Test 2 : " << solution("foobar") << endl;
+    vector<int> result1 = solution(test1);
+    cout << "Test 1 : [";
+    for (int num : result1) { // Range-based for loop
+        cout << num << ", ";
+    }
+    cout << "]" << endl;
+    cout << endl;
+
+    vector<int> result2 = solution(test2);
+    cout << "Test 2 : [";
+    for (int num : result2) { // Range-based for loop
+        cout << num << " ,";
+    }
+    cout << "]" << endl;
+    cout << endl;
 
     return 0;
 }
