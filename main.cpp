8fcf4d23ce7b580c700e523f4b3b20501a493ef2
Eichi2514 <picon1317@gmail.com>
2024-12-11 21:01:05 +0900

    프로그래머스) 명예의 전당 (1)

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+
 using namespace std;
 
-int solution(int a, int b, int n) {
-    int answer = 0;
+vector<int> solution(int k, vector<int> score) {
+    vector<int> answer;
 
-    while (n >= a) {
-        answer += n/a*b;
-        n = (n/a*b)+n%a;
+    for (int i = 0; i < score.size(); i++) {
+        vector<int> temps(score.begin(), score.begin() + i + 1);
+        sort(temps.begin(), temps.end());
+        if (i < k) answer.push_back(temps[0]);
+        else answer.push_back(temps[i-k+1]);
+
     }
 
     return answer;
@@ -16,8 +23,8 @@ int solution(int a, int b, int n) {
 int main() {
     // string test1 = "try hello world";
     // string test2 = "this is a test";
-    // vector<int> test1 = {1, 5, 2, 6, 3, 7, 4};
-    // vector<int> test2 = {1, 7, 1, 2};
+    vector<int> test1 = {10, 100, 20, 150, 1, 100, 200};
+    vector<int> test2 = {0, 300, 40, 300, 20, 70, 150, 50, 500, 1000};
     // vector<string> test1 = {"sun", "bed", "car"};
     // vector<string> test2 = {"abce", "abcd", "cdx"};
     // vector<vector<int> > test3 = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
@@ -26,24 +33,24 @@ int main() {
     // solution(test1,1);
     // solution(test2,2);
 
-    cout << "test1 : " << solution(2, 1 ,20) << endl;
-    cout << "test2 : " << solution(3, 1 ,20) << endl;
+    // cout << "test1 : " << solution(2, 1 ,20) << endl;
+    // cout << "test2 : " << solution(3, 1 ,20) << endl;
 
-    /*vector<int> result1 = solution(test1, test3);
+    vector<int> result1 = solution(3, test1);
     cout << "Test 1 : [";
     for (int temps : result1) { // Range-based for loop
         cout << temps << ", ";
     }
     cout << "]" << endl;
-    cout << endl;*/
+    cout << endl;
 
-    /*vector<int> result2 = solution(test2 , test4);
+    vector<int> result2 = solution(4, test2);
     cout << "Test 2 : [";
     for (int temps : result2) { // Range-based for loop
         cout << temps << " ,";
     }
     cout << "]" << endl;
-    cout << endl;*/
+    cout << endl;
 
     return 0;
 }
