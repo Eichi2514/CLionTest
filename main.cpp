eeffb99369cfc0cd4cc0babe07d9d2dbde8c9514
Eichi2514 <picon1317@gmail.com>
2024-12-11 20:40:12 +0900

    프로그래머스) 콜라 문제

--- main.cpp
+++ main.cpp
@@ -1,17 +1,11 @@
-#include <vector>
-#include <algorithm>
-
 using namespace std;
 
-vector<int> solution(vector<int> array, vector<vector<int>> commands) {
-    vector<int> answer(commands.size());
+int solution(int a, int b, int n) {
+    int answer = 0;
 
-    for (int i = 0; i < commands.size(); i++) {
-        vector<int> temps(array.begin() + commands[i][0] - 1, array.begin() + commands[i][1]);
-
-        sort(temps.begin(), temps.end());
-
-        answer[i] = temps[commands[i][2] - 1];
+    while (n >= a) {
+        answer += n/a*b;
+        n = (n/a*b)+n%a;
     }
 
     return answer;
@@ -22,26 +16,26 @@ vector<int> solution(vector<int> array, vector<vector<int>> commands) {
 int main() {
     // string test1 = "try hello world";
     // string test2 = "this is a test";
-    vector<int> test1 = {1, 5, 2, 6, 3, 7, 4};
+    // vector<int> test1 = {1, 5, 2, 6, 3, 7, 4};
     // vector<int> test2 = {1, 7, 1, 2};
     // vector<string> test1 = {"sun", "bed", "car"};
     // vector<string> test2 = {"abce", "abcd", "cdx"};
-    vector<vector<int> > test3 = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+    // vector<vector<int> > test3 = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
     // vector<vector<int> > test4 = {{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}};
 
     // solution(test1,1);
     // solution(test2,2);
 
-    // cout << "test1 : " << solution(test1) << endl;
-    // cout << "test2 : " << solution(test2) << endl;
+    cout << "test1 : " << solution(2, 1 ,20) << endl;
+    cout << "test2 : " << solution(3, 1 ,20) << endl;
 
-    vector<int> result1 = solution(test1, test3);
+    /*vector<int> result1 = solution(test1, test3);
     cout << "Test 1 : [";
     for (int temps : result1) { // Range-based for loop
         cout << temps << ", ";
     }
     cout << "]" << endl;
-    cout << endl;
+    cout << endl;*/
 
     /*vector<int> result2 = solution(test2 , test4);
     cout << "Test 2 : [";
