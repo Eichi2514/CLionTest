15f84ad21bce562cc90e078c5c4a0f986a07f5a9
Eichi2514 <picon1317@gmail.com>
2024-12-11 21:16:29 +0900

    프로그래머스) [1차] 비밀지도

--- main.cpp
+++ main.cpp
@@ -1,18 +1,20 @@
-#include <algorithm>
 #include <string>
 #include <vector>
+#include <bitset>
 
 using namespace std;
 
-vector<int> solution(int k, vector<int> score) {
-    vector<int> answer;
-
-    for (int i = 0; i < score.size(); i++) {
-        vector<int> temps(score.begin(), score.begin() + i + 1);
-        sort(temps.begin(), temps.end());
-        if (i < k) answer.push_back(temps[0]);
-        else answer.push_back(temps[i-k+1]);
-
+vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
+    vector<string> answer;
+    for (int i = 0; i < n; i++) {
+        string temp1 = bitset<32>(arr1[i]).to_string().substr(32 - n);
+        string temp2 = bitset<32>(arr2[i]).to_string().substr(32 - n);
+        string temp3 = "";
+        for (int j = 0; j < n; j++) {
+            if (temp1[j] == '1' || temp2[j] == '1') temp3 += '#';
+            else temp3 += ' ';
+        }
+        answer.push_back(temp3);
     }
 
     return answer;
@@ -23,8 +25,8 @@ vector<int> solution(int k, vector<int> score) {
 int main() {
     // string test1 = "try hello world";
     // string test2 = "this is a test";
-    vector<int> test1 = {10, 100, 20, 150, 1, 100, 200};
-    vector<int> test2 = {0, 300, 40, 300, 20, 70, 150, 50, 500, 1000};
+    vector<int> test1 = {9, 20, 28, 18, 11};
+    vector<int> test2 = {30, 1, 21, 17, 28};
     // vector<string> test1 = {"sun", "bed", "car"};
     // vector<string> test2 = {"abce", "abcd", "cdx"};
     // vector<vector<int> > test3 = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
@@ -36,21 +38,21 @@ int main() {
     // cout << "test1 : " << solution(2, 1 ,20) << endl;
     // cout << "test2 : " << solution(3, 1 ,20) << endl;
 
-    vector<int> result1 = solution(3, test1);
+    vector<string> result1 = solution(5, test1, test2);
     cout << "Test 1 : [";
-    for (int temps : result1) { // Range-based for loop
+    for (string temps : result1) { // Range-based for loop
         cout << temps << ", ";
     }
     cout << "]" << endl;
     cout << endl;
 
-    vector<int> result2 = solution(4, test2);
-    cout << "Test 2 : [";
-    for (int temps : result2) { // Range-based for loop
-        cout << temps << " ,";
-    }
-    cout << "]" << endl;
-    cout << endl;
+    // vector<int> result2 = solution(4, test2);
+    // cout << "Test 2 : [";
+    // for (int temps : result2) { // Range-based for loop
+    //     cout << temps << " ,";
+    // }
+    // cout << "]" << endl;
+    // cout << endl;
 
     return 0;
 }
