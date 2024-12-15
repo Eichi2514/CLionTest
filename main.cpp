15fa3339fa73705a11d54706e04c83c5d7cbb580
Eichi2514 <picon1317@gmail.com>
2024-12-15 19:57:11 +0900

    프로그래머스) 옹알이 (2)

--- main.cpp
+++ main.cpp
@@ -1,39 +1,56 @@
+#include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
-int solution(int n, int m, vector<int> section) {
-    int answer = 1;
-    int num = section[0];;
+int solution(vector<string> babbling) {
+    int answer = 0;
+    vector<string> answers = {"aya", "ye", "woo", "ma"};
 
-    for (int i = 0; i < section.size(); i++) {
-        if (num + m - 1 < section[i]) {
+    for (int i = 0; i < babbling.size(); i++) {
+        string a = "";
+
+        while (!babbling[i].empty()) {
+            bool b = false;
+            for (int j = 0; j < answers.size(); j++) {
+                if (babbling[i].find(answers[j]) == 0 && answers[j] != a) {
+                    babbling[i].erase(0, answers[j].size());
+                    a = answers[j];
+                    b = true;
+                    break;
+                }
+            }
+
+            if (!b) {
+                break;
+            }
+        }
+
+        if (babbling[i].empty()) {
             answer++;
-            num = section[i];
         }
     }
-
-    cout << "test1 : " << answer << endl;
 
     return answer;
 }
 
+#include <iostream>
+
 int main() {
     // string test1 = "try hello world";
     // string test2 = "this is a test";
-    vector<int> test1 = {2, 3, 6};
-    vector<int> test2 = {1, 3};
-    // vector<string> test1 = {"sun", "bed", "car"};
-    // vector<string> test2 = {"abce", "abcd", "cdx"};
+    // vector<int> test1 = {2, 3, 6};
+    // vector<int> test2 = {1, 3};
+    vector<string> test1 = {"aya", "yee", "yeye", "woowoo", "ma"};
+    vector<string> test2 = {"ayayeayayeaya", "mayewoo", "yemawoo"};
     // vector<vector<int> > test3 = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
     // vector<vector<int> > test4 = {{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}};
 
-    solution(8,4,test1);
-    solution(5,4,test2);
+    // solution(8,4,test1);
+    // solution(5,4,test2);
 
-    // cout << "test1 : " << solution(2, 1 ,20) << endl;
-    // cout << "test2 : " << solution(3, 1 ,20) << endl;
+    cout << "test1 : " << solution(test1) << endl;
+    cout << "test2 : " << solution(test2) << endl;
 
     // vector<string> result1 = solution(5, test1, test2);
     // cout << "Test 1 : [";
