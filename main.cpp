0e3ea502626a464ed81841cccb3f45866c9abbdd
Eichi2514 <picon1317@gmail.com>
2025-01-23 03:55:43 +0900

    코드업) 1281 : 홀수는 더하고 짝수는 빼고 3

--- main.cpp
+++ main.cpp
@@ -1,71 +1,25 @@
-#include <map>
-#include <string>
-#include <vector>
-
+#include <iostream>
 using namespace std;
 
-string solution(vector<string> survey, vector<int> choices) {
-    string answer = "";
-    map<char, int> scores;
-
-    for (int i = 0; i < choices.size(); i++) {
-        if (choices[i] == 4) continue;
-        char type1 = survey[i][0];
-        char type2 = survey[i][1];
-        int score = choices[i] - 4;
-
-        if (score < 0) {
-            scores[type1] -= score;
-        } else {
-            scores[type2] += score;
-        }
-    }
-
-    string types = "RTCFJMAN";
-    for (int i = 0; i < types.size(); i += 2) {
-        if (scores[types[i]] >= scores[types[i+1]]) {
-            answer += types[i];
-        } else {
-            answer += types[i+1];
-        }
-    }
-
-    return answer;
-}
-
-#include <iostream>
-
 int main() {
-    // string test1 = "try hello world";
-    // string test2 = "this is a test";
-    // vector<int> test1 = {2, 3, 6};
-    // vector<int> test2 = {1, 3};
-    // vector<string> test1 = {"aya", "yee", "yeye", "woowoo", "ma"};
-    // vector<string> test2 = {"ayayeayayeaya", "mayewoo", "yemawoo"};
-    // vector<vector<int> > test3 = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
-    // vector<vector<int> > test4 = {{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}};
+    int a, b;
+    int num = 0;
 
-    // solution(8,4,test1);
-    // solution(5,4,test2);
+    cin >> a >> b;  // 한 줄에 두 개의 정수 입력
 
-    // cout << "test1 : " << solution(test1) << endl;
-    // cout << "test2 : " << solution(test2) << endl;
+    for (int i = a; i <= b; i++) {
+        if (i % 2 == 0) {
+            cout << '-'<< i;
+            num -= i;
+        }
+        else {
+            if (i != a) cout << '+';
+            cout << i;
+            num += i;
+        }
+    }
 
-    // vector<string> result1 = solution(5, test1, test2);
-    // cout << "Test 1 : [";
-    // for (string temps : result1) { // Range-based for loop
-    //     cout << temps << ", ";
-    // }
-    // cout << "]" << endl;
-    // cout << endl;
-
-    // vector<int> result2 = solution(4, test2);
-    // cout << "Test 2 : [";
-    // for (int temps : result2) { // Range-based for loop
-    //     cout << temps << " ,";
-    // }
-    // cout << "]" << endl;
-    // cout << endl;
+    cout << '=' << num;
 
     return 0;
 }
