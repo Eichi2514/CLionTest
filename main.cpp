9652bf5d854b631d7fb9de6cb16be38aac24335b
Eichi2514 <picon1317@gmail.com>
2025-01-24 18:12:34 +0900

    코드업) 1285 : 계산기 2

--- main.cpp
+++ main.cpp
@@ -1,29 +1,43 @@
 #include <iostream>
-#include <cmath>
+#include <string>
 using namespace std;
 
-bool isPrime(int num) {
-    for (int i = 2; i <= sqrt(num); i++) {
-        if (num % i == 0) return false;
-    }
-    return true;
-}
-
 int main() {
-    int n;
-    cin >> n;
+    string words;
+    cin >> words;
 
-    for (int i = 2; i <= sqrt(n); i++) {
-        if (n % i == 0) {
-            int a = i;
-            int b = n / i;
-            if (isPrime(a) && isPrime(b)) {
-                cout << a << " " << b;
-                return 0;
-            }
+    int result = 0;
+    int num = 0;
+    char tmp = '+';
+    int index = 0;
+
+    while (index < words.length()) {
+        char a = words[index];
+
+        if (isdigit(a)) {
+            num = num * 10 + (a - '0');
         }
+
+        if (!isdigit(a) || index == words.length() - 1) {
+            if (tmp == '+') {
+                result += num;
+            } else if (tmp == '-') {
+                result -= num;
+            } else if (tmp == '*') {
+                result *= num;
+            } else if (tmp == '/') {
+                result /= num;
+            }
+
+            if (a == '=') break;
+
+            tmp = a;
+            num = 0;
+        }
+
+        index++;
     }
 
-    cout << "wrong number";
+    cout << result << endl;
     return 0;
 }
