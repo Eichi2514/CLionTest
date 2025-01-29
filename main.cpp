cf7ac9cdbe880a7a3603518f8bfef72e38c55406
Eichi2514 <picon1317@gmail.com>
2025-01-29 18:18:08 +0900

    코드업) 1289 : 가장 큰 운동장

--- main.cpp
+++ main.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int sub (int n) {
-    int sum = 1;
-    if (n == 0) return sum;
-    for (int i = 2; i <= n; i++) {
-        sum *= i;
-    }
-    return sum;
-}
-
 int main() {
-    int n, r;
-    cin >> n >> r;
+    int max = 0;
 
-    cout << sub(n) / (sub(r) * sub(n-r)) << endl;
+    for (int i = 0; i < 3; i++) {
+        int x, y;
+        cin >> x >> y;
+
+        max = max < x * y ? x * y : max;
+    }
+
+    cout << max << endl;
 
     return 0;
 }
