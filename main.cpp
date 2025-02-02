023b9e7bb2a3d9e9f331219d46fe604878eba4b6
Eichi2514 <picon1317@gmail.com>
2025-02-02 21:07:37 +0900

    1351 : 구구단 출력하기 2

--- main.cpp
+++ main.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n, m = 0, l = 0;
-    cin >> n;
-
-    for (int i = n / 2; i > 0; i--) {
-        if (i * (n - 2 * i) > l) {
-            l = i * (n - 2 * i);
-            m = i;
-        } else if (i * (n - 2 * i) == l && i < m) {
-            m = i;
-        }
+int solution(int num) {
+    for (int i = 1; i <= 9; i++) {
+        cout << num << "*" << i << "=" << num * i << endl;
     }
+    return 0;
+}
 
-    cout << m << endl;
+int main() {
+    int n, m;
+    cin >> n >> m;
+    for (int i = n; i <= m; i++) {
+        solution(i);
+    }
     return 0;
 }
