a22cfa15aadd6f2edcc68d7e9e24c0622609615d
Eichi2514 <picon1317@gmail.com>
2025-02-02 21:09:56 +0900

    코드업) 1352 : 사각형 출력하기 1

--- main.cpp
+++ main.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int solution(int num) {
-    for (int i = 1; i <= 9; i++) {
-        cout << num << "*" << i << "=" << num * i << endl;
-    }
-    return 0;
-}
-
 int main() {
-    int n, m;
-    cin >> n >> m;
-    for (int i = n; i <= m; i++) {
-        solution(i);
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << "*";
+        }
+        cout << endl;
     }
     return 0;
 }
