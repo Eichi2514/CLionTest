538cad8c79b1195a2b3dd2c5048dcd48e79bf39b
Eichi2514 <picon1317@gmail.com>
2025-02-03 19:12:51 +0900

    코드업) 1353 : 삼각형 출력하기 2

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    for (int i = 1; i <= n; i++) {
+    for (int i = n; i >= 1; i--) {
         for (int j = 0; j < i; j++) {
             cout << "*";
         }
